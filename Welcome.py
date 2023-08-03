7bab0b34118e8b6e015377fdfb50ee738750cba3
Steineklopferha <141242006+Steineklopferha@users.noreply.github.com>
2023-08-03 12:42:37 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -4,7 +4,7 @@ import os
 from bokeh.plotting import figure
 import numpy as np
 
-
+# find files
 file_name_list = []
 for i in os.listdir():
   if i.endswith('csv'): 
@@ -12,27 +12,38 @@ for i in os.listdir():
   
 st.write(file_name_list)
 
-st.write('Hello World!')
-location = st.selectbox('select location', file_name_list)  #to be able to select multiple files (multiselect)
+#st.write('Hello World!')
 
-df = pd.read_csv(location)
+# choose location and read data of it in
+location = st.multiselect('select location', file_name_list)  #to be able to select multiple files (multiselect)
+for i in location:
+  df = pd.read_csv(i)
 
-
-el_list = df.columns.tolist()[27:80]
-x_axis = st.selectbox('select element for x', el_list)
-y_axis = st.selectbox('select element for y', el_list)
-
-
-#df = pd.read_csv(location)
-p = figure(title=location + x_axis + ' vs. ' + y_axis, x_axis_label=x_axis + ' wt%', y_axis_label=y_axis + ' wt%')
-p.circle(df[x_axis]/10000, df[y_axis]/10000, size=5, color='red')
-
-mean = np.mean(df[y_axis]/10000)
-p.line(df[x_axis]/10000, mean, line_width=1.5, color="orange")
-
-std = np.std(df[y_axis]/10000)
-p.line(df[x_axis]/10000, mean-std, line_width=0.5, color="navy")
-p.line(df[x_axis]/10000, mean+std, line_width=0.5, color="navy")
-
-st.bokeh_chart(p, use_container_width=True)
+  # extract the elements and choose the ones for the x and y-axis
+  el_list = df.columns.tolist()[27:80]
+  x_axis = st.selectbox('select element for x', el_list)
+  y_axis = st.selectbox('select element for y', el_list)
+  
+  #calculations
+  
+  ##calculate the mean
+  mean = np.mean(df[y_axis]/10000)
+  
+  ##calculate the std
+  std = np.std(df[y_axis]/10000)
+  
+  
+  # plot 
+  ## the x vs. y data
+  p = figure(title=location + x_axis + ' vs. ' + y_axis, x_axis_label=x_axis + ' wt%', y_axis_label=y_axis + ' wt%')
+  p.circle(df[x_axis]/10000, df[y_axis]/10000, size=5, color='red')
+  
+  ## the mean line of y
+  p.line(df[x_axis]/10000, mean, line_width=1.5, color="orange")
+  
+  ## the std of y
+  p.line(df[x_axis]/10000, mean-std, line_width=0.5, color="navy")
+  p.line(df[x_axis]/10000, mean+std, line_width=0.5, color="navy")
+  
+  st.bokeh_chart(p, use_container_width=True)
 
