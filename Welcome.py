b91cc033771679aa2d3ea63182a59bf0499dd906
Steineklopferha <141242006+Steineklopferha@users.noreply.github.com>
2023-08-03 12:21:13 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -17,7 +17,7 @@ location = st.selectbox('select location', file_name_list)  #to be able to selec
 df = pd.read_csv(location)
 
 
-el_list = df.columns.tolist()[len(df)]
+el_list = df.columns.tolist()[range(len(df))]
 x_axis = st.selectbox('select element for x', el_list)
 y_axis = st.selectbox('select element for y', el_list)
 
