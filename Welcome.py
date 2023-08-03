5956393bdfec5d0959fdd9f6fabc47138b3ac604
Steineklopferha <141242006+Steineklopferha@users.noreply.github.com>
2023-08-03 12:16:20 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -12,18 +12,18 @@ for i in os.listdir():
 st.write(file_name_list)
 
 st.write('Hello World!')
+location = st.selectbox('select location', file_name_list)  #to be able to select multiple files (multiselect)
 
-df = pd.read_csv('Bastar Craton.csv')
+df = pd.read_csv(location)
 
 
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element for x', el_list)
 y_axis = st.selectbox('select element for y', el_list)
 
-location = st.selectbox('select location', file_name_list)  #to be able to select multiple files (multiselect)
 
-df = pd.read_csv(location)
-p = figure(title=location.drop('.csv') + x_axis + ' vs. ' + y_axis, x_axis_label=x_axis + ' wt%', y_axis_label=y_axis + ' wt%')
+#df = pd.read_csv(location)
+p = figure(title=location + x_axis + ' vs. ' + y_axis, x_axis_label=x_axis + ' wt%', y_axis_label=y_axis + ' wt%')
 p.circle(df[x_axis]/10000, df[y_axis]/10000, size=5, color='red')
 
 
