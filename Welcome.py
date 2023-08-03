b0fa3a9da2510ebda2b2e332db543a37d991358d
Steineklopferha <141242006+Steineklopferha@users.noreply.github.com>
2023-08-03 10:30:49 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -14,15 +14,15 @@ st.write(file_name_list)
 st.write('Hello World!')
 
 df = pd.read_csv('Bastar Craton.csv')
-st.dataframe(df)
+
 
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
 #y_axis = st.selectbox('select element', el_list)
 
-st.multiselect('select location', file_name_list,file_name_list[0])  #to be able to select multiple files
+location = st.selectbox('select location', file_name_list)  #to be able to select multiple files (multiselect)
 
-
+df = pd.read_csv(location)
 p = figure(x_axis_label='Mg wt%', y_axis_label='Si wt%')
 p.star(df['Mg']/10000, df['Si']/10000, size=10, color='red')
 
