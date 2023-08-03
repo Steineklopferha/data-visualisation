543822260f9137150ee2b715780fe5a2cb26064f
Steineklopferha <141242006+Steineklopferha@users.noreply.github.com>
2023-08-03 10:33:03 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -23,7 +23,7 @@ y_axis = st.selectbox('select element for y', el_list)
 location = st.selectbox('select location', file_name_list)  #to be able to select multiple files (multiselect)
 
 df = pd.read_csv(location)
-p = figure(x_axis_label='Mg wt%', y_axis_label='Si wt%')
-p.star(df['Mg']/10000, df['Si']/10000, size=10, color='red')
+p = figure(x_axis_label=x_axis + ' wt%', y_axis_label=y_axis + ' wt%')
+p.star(df[x_axis]/10000, df[y_axis]/10000, size=10, color='red')
 
 st.bokeh_chart(p, use_container_width=True)
